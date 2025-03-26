7570e4ac8d4aed84d8bbb3c886a2c229864b4385
Luis Benalcazar <lbenalcazar184@gmail.com>
2025-03-26 13:32:07 -0500

    6to

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,19 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 import { provideHttpClient } from '@angular/common/http';
+import { addIcons } from 'ionicons';
+
+import { playCircleOutline, pauseCircleOutline, stopCircleOutline } from 'ionicons/icons'
+
+
+
+addIcons({
+  'play-circle-outline': playCircleOutline,
+  'pause-circle-outline': pauseCircleOutline,
+  'stop-circle-outline': stopCircleOutline
+
+
+});
 
 
 bootstrapApplication(AppComponent, {
